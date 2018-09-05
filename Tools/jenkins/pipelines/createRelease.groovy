b4197557fcf54d1d3c104f9feb95600a8ca7e3ee
Kelly Johnson <kelly@leanplum.com>
2018-09-04 17:51:30 -0700

    Fix error in docker

--- Tools/jenkins/pipelines/createRelease.groovy
+++ Tools/jenkins/pipelines/createRelease.groovy
@@ -30,10 +30,10 @@ def createRelease() {
     def pythonImage = docker.build(
             'leanplum/android-tools-python',
             '-f ./Tools/jenkins/python.dockerfile .')
-    pythonImage.inside {
-        // Need git credentials to push the branch.
-        withCredentials(
-                [sshUserPrivateKey(credentialsId: 'e442ecf0-e843-4e89-b9bf-894e4eef5a61')]) {
+    // Need git credentials to push the branch.
+    withCredentials(
+            [sshUserPrivateKey(credentialsId: 'e442ecf0-e843-4e89-b9bf-894e4eef5a61')]) {
+        pythonImage.inside {
             sh 'make release TYPE=${TYPE}'
         }
     }
